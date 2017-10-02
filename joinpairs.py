cf098bdb9ab63775fde4b75ca59b5846bde59f53
fonsecamig <fonsecamig@gmail.com>
2017-10-02 22:53:34 +0100

    Add files via upload

--- joinpairs.py
+++ joinpairs.py
@@ -5,7 +5,7 @@ import csv
 #%%
 pairlist = ['XXBTZGBP', 'XXBTZUSD', 'XETHZUSD', 'XETHZGBP']
 
-filelist = [open(pair + '.csv', 'r') for pair in pairlist]
+filelist = [open('./kraken/' + pair + '.csv', 'r') for pair in pairlist]
 count = dict(zip(pairlist, [sum(1 for line in file)-1 for file in filelist]))
 for file in filelist: file.seek(0)
 csvlist = dict(zip(pairlist, [csv.DictReader(file) for file in filelist]))
@@ -17,25 +17,28 @@ time = pd.Series(np.NaN, index = pairlist)
 for pair in pairlist:
     line[pair], time[pair] = (np.NaN, a[pair]['time'])
 
-imin = time.argmax()
-line[imin] = a[pair]['price']
+imin = time.argmin()
+line[imin] = a[imin]['price']
 
-wfile = open('kraken_joint.csv', 'w')
+wfile = open('./kraken/kraken_joint.csv', 'w')
 wcsv = csv.DictWriter(wfile, fieldnames = pairlist + ['time'])
 wcsv.writeheader()
 
 while curridx != count:
-    imin = time.argmin()
-    a[imin] = next(csvlist[imin])
-    w = dict(zip(pairlist + ['time'], list(line) + [a[pair]['time']]))
+    imin = time.min()
+    ltime = list(time[time == imin].index)
+    for l in ltime:
+        a[l] = next(csvlist[l])
+        line[l] = a[l]['price']
+        curridx[l] += 1
+        if curridx[l] == count[l]:
+            time = time.drop(l)
+            line[l] = np.NaN
+        else:
+            time[l] = a[l]['time']
+            
+    w = dict(zip(pairlist + ['time'], list(line) + [a[ltime[0]]['time']]))
     wcsv.writerow(w)
-    line[imin] = a[imin]['price']
-    curridx[imin] += 1
-    if curridx[imin] == count[imin]:
-        time = time.drop(imin)
-        line[imin] = np.NaN
-    else:
-            time[imin] = a[imin]['time']
     
 for file in filelist: file.close()
 wfile.close()

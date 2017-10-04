a499d4ac7700f7b7583484d1a04a36e7df3893d9
fonsecamig <fonsecamig@gmail.com>
2017-10-04 17:18:36 +0100

    .

--- strat.py
+++ strat.py
@@ -1,23 +1,25 @@
 import datetime as dt
+import pandas as pd
 import numpy as np
 
 #%%
-class strategy(object):
+class Strategy(object):
     """
     
     """
     
-    def indicatorAdv(self, pos, time):
+    def indicator_adv(self, pos, time):
         pass
     
     def advice(self, pos, time):
         if pos.status == 'o':
-            return(self.indicatorAdv(pos, time))
-        
+            return self.indicator_adv(pos, time)
 
+#%%
 #%%
 IKH = strategy()
 
-def ind(time, log):
-    daybef = dt.utcfromtimestamp(dt.datetime.utcfromtimestamp(time) - dt.timedelta(seconds = time))
-    np.searchsorted(daybef)
+def ind(time, log, period = 86400):
+    postenkansen = argmin(time - 26 * period)
+    if
+

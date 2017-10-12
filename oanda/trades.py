7cd7ce9c776b75a8c4da818e524d65d2bcf74707
fonsecamig <fonsecamig@gmail.com>
2017-10-12 21:49:27 +0100

    .
    Signed-off-by: fonsecamig <fonsecamig@gmail.com>

---
+++ oanda/trades.py
@@ -0,0 +1,75 @@
+# use of the Trades{..} classes
+import json
+import requests
+from oandapyV20 import API
+
+
+import oandapyV20.endpoints.trades as trades
+import sys
+
+def exampleAuth():
+    accountID, token = None, None
+    with open("oanda.txt") as I:
+        accountID = I.readline().strip()
+        token = I.readline().strip()
+    return accountID, token
+
+accountID, access_token = exampleAuth()
+
+chc = sys.argv[1]
+
+api = API(access_token=access_token)
+
+if chc == 'list':
+   r = trades.TradesList(accountID)
+   rv = api.request(r)
+   print("RESP:\n{} ".format(json.dumps(rv, indent=2)))
+
+if chc == 'open':
+   r = trades.OpenTrades(accountID)
+   rv = api.request(r)
+   print("RESP:\n{} ".format(json.dumps(rv, indent=2)))
+   tradeIDs = [o["id"] for o in rv["trades"]]
+   print("TRADE IDS: {}".format(tradeIDs))
+
+if chc == 'details':
+   for O in sys.argv[2:]:
+       r = trades.TradeDetails(accountID, tradeID=O)
+       rv = api.request(r)
+       print("RESP:\n{} ".format(json.dumps(rv, indent=2)))
+
+if chc == 'close':
+   X = iter(sys.argv[2:])
+   for O in X:
+       cfg = { "units": X.next() }
+       r = trades.TradeClose(accountID, tradeID=O, data=cfg)
+       rv = api.request(r)
+       print("RESP:\n{} ".format(json.dumps(rv, indent=2)))
+
+if chc == 'cltext':
+   for O in sys.argv[2:]:  # tradeIDs
+       cfg = { "clientExtensions": {
+               "id": "myID{}".format(O),
+               "comment": "myComment",
+            }
+         }
+       r = trades.TradeClientExtensions(accountID, tradeID=O, data=cfg)
+       rv = api.request(r)
+       print("RESP:\n{} ".format(json.dumps(rv, indent=2)))
+
+if chc == 'crc_do':
+   X = iter(sys.argv[2:])
+   for O in X:
+       cfg = {
+               "takeProfit": {
+                 "timeInForce": "GTC",
+                 "price": X.next(),
+               },
+               "stopLoss": {
+                 "timeInForce": "GTC",
+                 "price": X.next()
+               }
+         }
+       r = trades.TradeCRCDO(accountID, tradeID=O, data=cfg)
+       rv = api.request(r)
+       print("RESP:\n{} ".format(json.dumps(rv, indent=2)))

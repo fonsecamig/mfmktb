2fd89eefdb575ab1b25a5408086546a712993894
fonsecamig <fonsecamig@gmail.com>
2017-10-19 20:50:16 +0100

    .
    Signed-off-by: fonsecamig <fonsecamig@gmail.com>

--- oanda/oanda.py
+++ oanda/oanda.py
@@ -9,6 +9,7 @@ from oandapyV20.contrib.requests import (
 )
 
 import oandapyV20.endpoints.orders as orders
+import oandapyV20.endpoints.trades as trades
 import oandapyV20
 
 #from exampleauth import exampleAuth
@@ -21,42 +22,74 @@ def exampleAuth():
         token = I.readline().strip()
     return accountID, token
 
-# -*- coding: utf-8 -*-
-"""Example demonstrating the contrib.request classes.
-
-Create a MarketOrderRequest to enter 10000 EUR_USD LONG position along with
-- a TakeProfitOrder to take profit @1.10
-- a StopLossOrder to take loss @1.07
-
-These values apply for this moment: EUR_USD 1.0605
-So when you run the example you may need to change the values.
-"""
-
-
+# # -*- coding: utf-8 -*-
+# """Example demonstrating the contrib.request classes.
+#
+# Create a MarketOrderRequest to enter 10000 EUR_USD LONG position along with
+# - a TakeProfitOrder to take profit @1.10
+# - a StopLossOrder to take loss @1.07
+#
+# These values apply for this moment: EUR_USD 1.0605
+# So when you run the example you may need to change the values.
+# """
+#
+#
 accountID, access_token = exampleAuth()
-api = oandapyV20.API(access_token=access_token)
+# api = oandapyV20.API(access_token=access_token)
+#
+# # EUR_USD (today 1.0605)
+# EUR_USD_STOP_LOSS = 1.16
+# EUR_USD_TAKE_PROFIT = 1.21
+#
+# # The orderspecs
+# mktOrder = MarketOrderRequest(
+#     instrument="EUR_USD",
+#     units=10000,
+#     takeProfitOnFill=TakeProfitDetails(price=EUR_USD_TAKE_PROFIT).data,
+#     stopLossOnFill=StopLossDetails(price=EUR_USD_STOP_LOSS).data
+# )
+#
+# print("Market Order specs: \n{}".format(json.dumps(mktOrder.data, indent=4)))
+#
+# # create the OrderCreate request
+# r = orders.OrderCreate(accountID, data=mktOrder.data)
+#
+# try:
+#     # create the OrderCreate request
+#     rv = api.request(r)
+# except oandapyV20.exceptions.V20Error as err:
+#     print(r.status_code, err)
+# else:
+#     print(json.dumps(rv, indent=2))
 
-# EUR_USD (today 1.0605)
-EUR_USD_STOP_LOSS = 1.16
-EUR_USD_TAKE_PROFIT = 1.21
+data0 = \
+{
+  "order": {
+    "price": "1.2",
+    "stopLossOnFill": {
+      "timeInForce": "GTC",
+      "price": "1.22"
+    },
+    "timeInForce": "GTC",
+    "instrument": "EUR_USD",
+    "units": "100",
+    "type": "LIMIT",
+    "positionFill": "DEFAULT"
+  }
+}
 
-# The orderspecs
-mktOrder = MarketOrderRequest(
-    instrument="EUR_USD",
-    units=10000,
-    takeProfitOnFill=TakeProfitDetails(price=EUR_USD_TAKE_PROFIT).data,
-    stopLossOnFill=StopLossDetails(price=EUR_USD_STOP_LOSS).data
-)
+data1 = \
+{
+  "order": {
+    "units": "-100",
+    "instrument": "EUR_USD",
+    "timeInForce": "FOK",
+    "type": "MARKET",
+    "positionFill": "OPEN_ONLY"
+  }
+}
 
-print("Market Order specs: \n{}".format(json.dumps(mktOrder.data, indent=4)))
-
-# create the OrderCreate request
-r = orders.OrderCreate(accountID, data=mktOrder.data)
-
-try:
-    # create the OrderCreate request
-    rv = api.request(r)
-except oandapyV20.exceptions.V20Error as err:
-    print(r.status_code, err)
-else:
-    print(json.dumps(rv, indent=2))
+client = oandapyV20.API(access_token=access_token)
+r = orders.OrderCreate(accountID, data=data1)
+client.request(r)
+print(r.response)
---
+++ oanda/translator.py
@@ -0,0 +1,47 @@
+import oandapyV20
+
+class Translate(object):
+    """
+
+    """
+
+    def __init__(self, broker, accountID, token):
+        self.broker = broker
+        self.accountID = accountID
+        self.token = token
+
+    def open(self, type, pair, price, vol, slip):
+        if self.broker = "oanda":
+            if type = 'l':
+                priceB = (1 - slip) * price
+            if type = 's':
+                priceB = (1 + slip) * price
+            data = \
+                {
+                    "order": {
+                        "units": str(vol),
+                        "instrument": pair,
+                        "timeInForce": "FOK",
+                        "type": "MARKET",
+                        "positionFill": "OPEN_ONLY",
+                        "priceBound": str(priceB)
+                    }
+                }
+            r = orders.OrderCreate(self.accountID, data = data)
+            client.request(r)
+            return(r.response)
+
+    def close(self, pos):
+        if self.broker = "oanda":
+            if pos.status = 'w':
+                orders.OrderCancel(accountID = self.accountID, orderID=posID)
+                client.request(r)
+                return (r.response)
+            if pos.tatus = 'o':
+                data = \
+                {
+                    "units": 100
+                }
+                r = trades.TradeClose(accountID=self.accountID, data=data)
+                client.request(r)
+                print(r.response)

d66902d1e851aae3f7b6739f03f546658cac4d03
fonsecamig <fonsecamig@gmail.com>
2017-10-25 17:29:05 +0100

    .
    Signed-off-by: fonsecamig <fonsecamig@gmail.com>

--- trade.py
+++ trade.py
@@ -41,42 +41,47 @@ class Translator(object):
             r = orders.OrderCreate(self.accountID, data = data)
             self.client.request(r)
             reply = dict(r.response)
-            print(reply)
             if "orderFillTransaction" in reply.keys():
                 OFT = reply["orderFillTransaction"]
                 print(OFT)
                 posID = OFT["tradeOpened"]["tradeID"]
                 iprice = float(OFT["price"])
-                ivol = float(OFT["tradeOpened"]["units"])
-                t =
+                ivol = float(OFT["units"])
+                t = pd.Timestamp(OFT["time"], tzinfo = 'UTC')
                 pos = Position(self.broker, posID, pair, iprice, ivol, type, t)
+                pos.status = 'o'
+                pos.trans = self
                 return(pos)
             else:
                 return(None)
 
     def close(self, pos, vol):
-        if pos.broker == "oanda":
-            # if pos.status == 'w':
-            #     r = orders.OrderCancel(accountID = self.accountID, orderID = posID)
-            #     client.request(r)
-            #     reply = dict(r.response)
-            #     v = pos.orderLog.iloc[-1, 'volLeft']
-            #     p = pos.orderLog.iloc[-1, 'price']
-            #     t = pd.to_datetime(reply["orderCancelTransaction"]["time"])
-            #     pos.orderLog.loc[t] = pd.DataFrame({'volLeft': v, 'price': p})
-            data = \
-            {
-                "units": str(vol)
-            }
-            r = trades.TradeClose(accountID = self.accountID, data = data)
-            self.client.request(r)
-            reply = dict(r.response)
-            v = pos.log.iloc[-1,].at['vol'] - float(reply["OrderFillTransaction"]["units"])
-            p = float(reply["OrderFillTransaction"]["price"])
-            cp = pos.log.iloc[-1,].at['closedprofit'] + reply["OrderFillTransaction"]["pl"]
-            t = pd.Timestamp(pd.to_datetime(reply["OrderFillTransaction"]["time"]))
-            pos.log.loc[t] = pd.DataFrame({'vol': [v], 'price': [p], 'closedprofit': [cp]})
-            return(r.response)
+        if vol <= pos.log.iloc[-1,].at['vol']:
+            if pos.broker == "oanda":
+                # if pos.status == 'w':
+                #     r = orders.OrderCancel(accountID = self.accountID, orderID = posID)
+                #     client.request(r)
+                #     reply = dict(r.response)
+                #     v = pos.orderLog.iloc[-1, 'volLeft']
+                #     p = pos.orderLog.iloc[-1, 'price']
+                #     t = pd.to_datetime(reply["orderCancelTransaction"]["time"])
+                #     pos.orderLog.loc[t] = pd.DataFrame({'volLeft': v, 'price': p})
+                data = \
+                {
+                    "units": str(vol)
+                }
+                r = trades.TradeClose(accountID = self.accountID, tradeID = pos.posID, data = data)
+                self.client.request(r)
+                reply = dict(r.response)
+                OFT = reply["orderFillTransaction"]
+                print(OFT)
+                v = pos.log.iloc[-1,].at['vol'] + float(OFT["units"])
+                p = float(OFT["price"])
+                cp = pos.log.iloc[-1,].at['closedprof'] + float(OFT["pl"])
+                t = pd.Timestamp(pd.Timestamp(OFT["time"]))
+                pos.log.loc[t] = {'vol': v, 'price': p, 'closedprof': cp}
+                pos.status = 'c'
+                return(r.response)
 
 #%%
 class Position(object):
@@ -91,7 +96,7 @@ class Position(object):
         self.pair = pair
         self.initPrice = initPrice #open price
         self.initVol = initVol #initial position volume
-        self.log = pd.DataFrame({'vol': [0], 'price': [initPrice], 'closedprof': [0]}, index = [t])
+        self.log = pd.DataFrame({'vol': [initVol], 'price': [initPrice], 'closedprof': [float(0)]}, index = [t])
         self.orderLog = pd.DataFrame({'volLeft': [initVol], 'price': [initPrice]}, index = [t])
         self.tradeVol = 0
         self.typePos = typePos #l: long, s: short
@@ -103,13 +108,14 @@ class Position(object):
         self.stopLoss=stopLoss #hard coded stop loss broker side
         self.takeProfit=takeProfit #hard coded take profit broker side
         # self.log = pd.Series([np.NaN, np.NaN],index = ['entry', 'exit'])
-        
+
+
     def profitcalc(self, price):
         if self.typePos == 'l':
             return(self.log.closedprof[-1] + self.tradeVol * price - self.cost)
         if self.typePos == 's':
             return(self.log.closedprof[-1] + self.cost - self.tradeVol * price)
-    
+
     def tick(self, t, price):
 #        if self.status == 'o':
 #            self.log.append([t, price])
@@ -122,7 +128,7 @@ class Position(object):
     #     self.typePos=typePos
     #     self.profit = price - self.initPrice
         
-    def closePos(self, t, vol, price):
+    def closePos(self, vol = self.log.closedprof[-1]):
         self.trans.close(self, vol)
         # if self.status == 'o':
         #     if vol <= self.tradeVol:

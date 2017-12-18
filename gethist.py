64545554117a9690a76a0ed3c268e8c64e393c3b
Miguel Fonseca <fonsecamig@gmail.com>
2017-12-18 22:38:07 +0000

    .

--- gethist.py
+++ gethist.py
@@ -18,6 +18,8 @@ transl = td.Translator()
 
 cfg.pairList = ["EUR_USD", "GBP_USD", "USD_CAD", "GBP_CHF"]
 
+transl.initAccount('oanda', access_token)
+
 for pair in cfg.pairList:
     htab = transl.histPrice('oanda', 0, 'EUR_USD', pd.Timestamp('2016-01-01T00:00:00'), gran='S5')
     htab.to_csv(pair + ".csv")

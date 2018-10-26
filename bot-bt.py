d01f1c052005a6181e8dadf8d12ee6d813397d4d
Miguel Fonseca <fonsecamig@gmail.com>
2018-10-26 19:03:27 +0100

    .

--- bot-bt.py
+++ bot-bt.py
@@ -11,9 +11,9 @@ transl = td.Translator()
 # cfg.pairList = ["EUR_USD", "GBP_USD"]
 # strategy = stratg.TestStrategy(10, 0.01, 0.00025, 0.5)
 strategy = stratg.Gasoline(10, 0.01, 0.00025, 0.5, 2)
-dur = pd.Timedelta(seconds = 5)
+dur = pd.Timedelta(seconds=5)
 
-transl.initAccount('backtest', 0, btpath = './bt/', btfilelist = ['EURUSD-2017-11.csv', 'USDJPY-2017-11.csv'])
+transl.initAccount('backtest', 0, btpath='./bt/', btfilelist=['EURUSD-2017-11.csv', 'USDJPY-2017-11.csv'])
 
 for broker in cfg.brokerList:
     for acc in range(cfg.brokerList[broker]['accounts'].__len__()):
@@ -22,7 +22,7 @@ for broker in cfg.brokerList:
         print(cfg.priceList[broker]['accounts'][acc])
 
 strategy.initiate()
-timeStop = pd.Timestamp.now(tz = 'utc') + dur
+timeStop = pd.Timestamp.now(tz='utc') + dur
 while cfg.brokerList['backtest']['accounts'][0]['psv'] != []:
     for broker in cfg.brokerList:
         for acc in range(cfg.brokerList[broker]['accounts'].__len__()):
@@ -32,12 +32,13 @@ while cfg.brokerList['backtest']['accounts'][0]['psv'] != []:
                 print([p.log for p in cfg.posList])
                 print(strategy.advice())
                 transl.execute(strategy.advice())
-            except (ConnectionResetError, requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.HTTPError, requests.exceptions.ChunkedEncodingError):
+            except (ConnectionResetError, requests.exceptions.Timeout, requests.exceptions.ConnectionError,
+                    requests.exceptions.HTTPError, requests.exceptions.ChunkedEncodingError):
                 transl.initPosLog(broker, acc)
                 transl.initTick(broker, acc)
 
 posLogL = [p.log for p in cfg.posList]
-posLog = pd.concat(posLogL, keys = list(range(1, posLogL.__len__() + 1)))
+posLog = pd.concat(posLogL, keys=list(range(1, posLogL.__len__() + 1)))
 with pd.ExcelWriter('positions.xlsx') as outfile:
     posLog.to_excel(outfile)
 
@@ -50,6 +51,6 @@ for broker in cfg.brokerList:
         bIndex.append((ind + 1, acc + 1))
     ind += 1
 index = pd.MultiIndex.from_tuples(bIndex, names=['broker', 'account'])
-brokerLog = pd.concat(brokerL, keys = index)
+brokerLog = pd.concat(brokerL, keys=index)
 with pd.ExcelWriter('accounts.xlsx') as outfile:
     brokerLog.to_excel(outfile)
--- strategies.py
+++ strategies.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 import config as cfg
 
+
 class TestStrategy(object):
     """
 
@@ -22,24 +23,31 @@ class TestStrategy(object):
         for broker in cfg.brokerList:
             for acc in range(cfg.brokerList[broker]["accounts"].__len__()):
                 for pair in cfg.pairList:
-                    pList = [cfg.posList.index(pos) for pos in cfg.posList if pos.broker == broker and pos.account == acc and pos.pair == pair and pos.status == 'o']
+                    pList = [cfg.posList.index(pos) for pos in cfg.posList if
+                             pos.broker == broker and pos.account == acc and pos.pair == pair and pos.status == 'o']
                     print(pList)
                     if pList == []:
-                        orders.append({'oType': 'o', 'broker': broker, 'account': acc, 'type': 'l', 'pair': pair, 'vol': self.volTest,
-                                       'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['ask'], 'slip': self.slip})
+                        orders.append({'oType': 'o', 'broker': broker, 'account': acc, 'type': 'l', 'pair': pair,
+                                       'vol': self.volTest,
+                                       'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['ask'],
+                                       'slip': self.slip})
                     elif pList.__len__() == 1:
-                        if cfg.posList[pList[0]].tick() / (cfg.posList[pList[0]].initPrice * cfg.posList[pList[0]].initVol) - 1 < -self.perProfit:
+                        if cfg.posList[pList[0]].tick() / (
+                                cfg.posList[pList[0]].initPrice * cfg.posList[pList[0]].initVol) - 1 < -self.perProfit:
                             orders.append(
                                 {'oType': 'c', 'broker': broker, 'account': acc, 'pos': pList[0], 'vol': self.volTest})
                             if cfg.posList[pList[0]].typePos == 'l':
                                 tPos = 's'
                             else:
-                                tPos ='l'
+                                tPos = 'l'
                             orders.append({'oType': 'o', 'broker': broker, 'account': acc, 'type': tPos, 'pair': pair,
                                            'vol': self.volTest,
-                                           'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['ask'], 'slip': self.slip})
+                                           'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['ask'],
+                                           'slip': self.slip})
                     else:
-                        profitLPer = pd.Series([cfg.posList[p].tick() / (cfg.posList[p].initPrice * cfg.posList[p].initVol) - 1 for p in pList])
+                        profitLPer = pd.Series(
+                            [cfg.posList[p].tick() / (cfg.posList[p].initPrice * cfg.posList[p].initVol) - 1 for p in
+                             pList])
                         print(profitLPer)
                         if profitLPer.max() > self.perProfit:
                             if cfg.posList[pList[-1]] == 'l':
@@ -48,11 +56,65 @@ class TestStrategy(object):
                                 priceT = 'bid'
 
                             orders.append(
-                                {'oType': 'o', 'broker': broker, 'account': acc, 'type': pList[-1].typePos, 'pair': pair, 'vol': self.volTest,
+                                {'oType': 'o', 'broker': broker, 'account': acc, 'type': pList[-1].typePos,
+                                 'pair': pair, 'vol': self.volTest,
                                  'price': cfg.priceList[broker][acc].loc[pair][priceT], 'slip': self.slip})
                         elif profitLPer.sum() < max([pList.__len__() - 1, 1]) * self.perProfit * self.perLoss:
                             for pos in pList:
                                 orders.append(
                                     {'oType': 'c', 'broker': broker, 'account': acc, 'pos': pos, 'vol': self.volTest})
                         # print([(p.status, p.log) for p in pList])
-        return(orders)
+        return (orders)
+
+
+class TestPredictionStrategy(object):
+    """
+
+    """
+
+    def __init__(self, vol, slip, perProfit, perLoss):
+        self.vol = vol
+        self.slip = slip
+        self.perProfit = perProfit
+        self.perLoss = perLoss
+
+    def initiate(self):
+        pass
+
+    def advice(self):
+        orders = []
+        for broker in cfg.brokerList:
+            for acc in range(cfg.brokerList[broker]["accounts"].__len__()):
+                for pair in cfg.pairList:
+                    pList = [cfg.posList.index(pos) for pos in cfg.posList if
+                             pos.broker == broker and pos.account == acc and pos.pair == pair and pos.status == 'o']
+                    maxHigh = cfg.pred[pair][:][1].max()
+                    minLow = cfg.pred[pair][:][2].min()
+                    listLong = [pos for pos in pList if cfg.posList[pos].typePos == 'l']
+                    listShort = [pos for pos in pList if cfg.posList[pos].typePos == 's']
+                    if listLong == []:
+                        if maxHigh > cfg.priceList[broker]['accounts'][acc].loc[pair]['ask']:
+                            orders.append(
+                                {'oType': 'o', 'broker': broker, 'account': acc, 'type': 'l', 'pair': pair,
+                                 'vol': self.volTest,
+                                 'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['ask'],
+                                 'slip': self.slip})
+                    else:
+                        for pos in listLong:
+                            if maxHigh < cfg.priceList[broker]['accounts'][acc].loc[pair]['ask']:
+                                orders.append(
+                                    {'oType': 'c', 'broker': broker, 'account': acc, 'pos': pList[pos],
+                                     'vol': self.vol})
+                    if listShort == []:
+                        if minLow < cfg.priceList[broker]['accounts'][acc].loc[pair]['bid']:
+                            orders.append(
+                                {'oType': 'o', 'broker': broker, 'account': acc, 'type': 's', 'pair': pair,
+                                 'vol': self.volTest,
+                                 'price': cfg.priceList[broker]['accounts'][acc].loc[pair]['bid'],
+                                 'slip': self.slip})
+                        else:
+                            for pos in listLong:
+                                if minLow > cfg.priceList[broker]['accounts'][acc].loc[pair]['ask']:
+                                    orders.append(
+                                        {'oType': 'c', 'broker': broker, 'account': acc, 'pos': pList[pos],
+                                         'vol': self.vol})
